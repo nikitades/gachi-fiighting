7a080400892edb5acf89d79df0bc65b569a988b1
Nikita Pavlovskiy <nikita.pavlovskiy@carvago.com>
2020-11-22 20:18:21 +0300

    The initial commit

---
+++ backend/GachiFighting.Matchmaking/ChatHub.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GachiFighting.Matchmaking
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await this.Clients.All.SendAsync("Send", "zhopa");
+        }
+    }
+}
